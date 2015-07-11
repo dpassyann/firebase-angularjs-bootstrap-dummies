c3e0d38667bbd3585af5aaeb206c7a2d0918d26c
dpassyann <deungoueyann@gmail.com>
2015-07-12 01:35:57 +0200

    creation du service d'utilisateur : authentification

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -4,7 +4,14 @@
 'use strict';
 
 app
-    .controller('index', function ($scope, userProvider) {
+    .controller('index', function ( $scope ) {
 
+    })
+
+    .controller('indexCreateUser', function ( $scope, userProvider, $location ) {
+        $scope.create = function( user ){
+            userProvider.create( user );
+            $location.url( '/' );
+        }
     })
 ;
--- src/services/userProvider.js
+++ src/services/userProvider.js
@@ -4,19 +4,37 @@
 
 'use strict';
 
-app.service('userProvider', function (user) {
+app.service('userProvider', function () {
 
     var ref = new Firebase("https://boiling-heat-1339.firebaseio.com/");
 
-    ref.createUser({
-        email    : user.email,
-        password : user.password
-    }, function(error, userData) {
-        if (error) {
-            console.log("Error creating user:", error);
-        } else {
-            console.log("Successfully created user account with uid:", userData.uid);
-        }
-    });
+    this.create = function( user ){
+        ref.createUser({
+            email    : user.email,
+            password : user.password
+        }, function(error, userData) {
+            if (error) {
+                console.log("Error creating user:", error);
+            } else {
+                console.log("Successfully created user account with uid:", userData.uid);
+            }
+        });
+    }
+
+    this.connect = function( user ){
+        ref.authWithPassword({
+            email    : user.email,
+            password : user.password
+        }, function(error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+            }
+        }, {
+            remember:sessionOnly
+        })
+    }
+
 
 });
